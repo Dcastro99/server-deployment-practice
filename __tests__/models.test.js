bc1d4d5abd75d53b64ac079e2e21bd9aa4d11c8a
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-07-20 15:17:47 -0700

    created model test

--- __tests__/models.test.js
+++ __tests__/models.test.js
@@ -10,48 +10,77 @@ describe('models', () => {
   beforeEach(async () => {
     await db.sync();
   });
-  describe('Musician', () => {
-    beforeEach(async () => {
-      await db.sync();
-    });
-    let musicianType;
-    it('can list a musician', async () => {
-      let listMusicianRes = await request.get('/musician');
-      expect(listMusicianRes.status).toBe(200);
-      expect(listMusicianRes.body[0]).toHaveProperty('musicianType');
-    });
-    //not sure why github wont pass "list" test
 
-    it('can update a musician', async () => {
-      const updateRes = await request.put(`/musician/${musicianType}`);
-      expect(updateRes.status).toBe(200);
-    });
-    it('can delete a musician', async () => {
-      const deleteRes = await request.delete(`/musician/${musicianType}`);
-      expect(deleteRes.status).toBe(200);
+  // CREATES MUSICIAN
+  it('creates a miusician', async () => {
+    let response = await request.post('/musician').send({
+      musicianType: 'Test musician',
+      instrument: 'string',
     });
 
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      musicianType: 'Test musician',
+      instrument: 'string',
+    });
   });
 
-  describe('Golfer', () => {
-    beforeEach(async () => {
-      await db.sync();
-    });
-    let golferName;
-    it('can list a golfer', async () => {
-      let listGolferRes = await request.get('/golfer');
-      expect(listGolferRes.status).toBe(200);
-      expect(listGolferRes.body[0]).toHaveProperty('golferName');
-    });
-    it('can update a golfer', async () => {
-      const upRes = await request.put(`/golfer/${golferName}`);
-      expect(upRes.status).toBe(200);
-    });
-    it('can delete a golfer', async () => {
-      const delRes = await request.delete(`/golfer/${golferName}`);
-      expect(delRes.status).toBe(200);
+  //UPDATES MUSICIAN
+  it('retrieves a musician', async () => {
+    let createResponse = await request.post('/musician').send({
+      musicianType: 'Test musician',
+      instrument: 'string',
     });
 
+    expect(createResponse.status).toBe(200);
+    const createdId = createResponse.body.id;
 
+    let retrieveResponse = await request.get(`/musician/${createdId}`);
+
+    expect(retrieveResponse.status).toBe(200);
+    expect(retrieveResponse.body).toMatchObject({
+      id: createdId,
+      musicianType: 'Test musician',
+      instrument: 'string',
+    });
   });
+
+  //IT LISTS MUSICIAN
+  let musicianType;
+  it('can list a musician', async () => {
+    let listMusicianRes = await request.get('/musician');
+    expect(listMusicianRes.status).toBe(200);
+    expect(listMusicianRes.body[0]).toHaveProperty('musicianType');
+  });
+  //not sure why github wont pass "list" test
+
+  it('can update a musician', async () => {
+    const updateRes = await request.put(`/musician/${musicianType}`);
+    expect(updateRes.status).toBe(200);
+  });
+  it('can delete a musician', async () => {
+    const deleteRes = await request.delete(`/musician/${musicianType}`);
+    expect(deleteRes.status).toBe(200);
+  });
+
+
+
+
+  let golferName;
+  it('can list a golfer', async () => {
+    let listGolferRes = await request.get('/golfer');
+    expect(listGolferRes.status).toBe(200);
+    expect(listGolferRes.body[0]).toHaveProperty('golferName');
+  });
+  it('can update a golfer', async () => {
+    const upRes = await request.put(`/golfer/${golferName}`);
+    expect(upRes.status).toBe(200);
+  });
+  it('can delete a golfer', async () => {
+    const delRes = await request.delete(`/golfer/${golferName}`);
+    expect(delRes.status).toBe(200);
+  });
+
+
+
 });
